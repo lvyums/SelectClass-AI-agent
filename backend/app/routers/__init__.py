853b00a8784ae98efb1b39bf36db100fbb299edf
lvyums <ly66st@163.com>
2026-06-06 14:53:40 +0800

    删除不必要文件

--- backend/app/routers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .auth import router as auth
-from .profile import router as profile
-from .courses import router as courses
-from .recommendations import router as recommendations
-from .chat import router as chat
-from .selection import router as selection
